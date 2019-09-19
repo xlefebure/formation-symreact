6ec8a3f7d0858e1d8d4160b1100bf4eee07facbc
Xavier <xavier.lefebure@gmail.com>
2019-09-19 15:55:31 +0200

    Modification du gitignore et ajout des clés PEM

--- assets/js/services/AuthAPI.js
+++ assets/js/services/AuthAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import {LOGIN_API} from "../config";
 
 /**
  * Déconnexion (suppression du token du localStorage & Axios
@@ -17,7 +18,7 @@ function logout() {
  */
 function authenticate(credentials) {
     return axios
-        .post("http://127.0.0.1:8000/api/login_check", credentials)
+        .post(LOGIN_API, credentials)
         .then(response => response.data.token)
         .then(token => {
             // Je stocke le token dans mon localStorage
--- assets/js/services/InvoicesAPI.js
+++ assets/js/services/InvoicesAPI.js
@@ -1,28 +1,29 @@
 import axios from "axios";
+import {INVOICES_API} from "../config";
 
 function findAll() {
     return axios
-        .get("http://127.0.0.1:8000/api/invoices")
+        .get(INVOICES_API)
         .then(response => response.data['hydra:member'])
 }
 
 function find(id) {
     return axios
-        .get("http://127.0.0.1:8000/api/invoices/" + id)
+        .get(INVOICES_API + "/" + id)
         .then(response => response.data);
 }
 
 function update(id, invoice) {
-    return axios.put("http://127.0.0.1:8000/api/invoices/" + id, {...invoice, customer: `/api/customers/${invoice.customer}`});
+    return axios.put(INVOICES_API + "/" + id, {...invoice, customer: `/api/customers/${invoice.customer}`});
 }
 
 function create(invoice) {
-    return axios.post("http://127.0.0.1:8000/api/invoices", {...invoice, customer: `/api/customers/${invoice.customer}`});
+    return axios.post(INVOICES_API, {...invoice, customer: `/api/customers/${invoice.customer}`});
 }
 
 function deleteInvoice(id) {
     axios
-        .delete("http://127.0.0.1:8000/api/invoices/" + id)
+        .delete(INVOICES_API + "/" + id)
 }
 
 export default {
